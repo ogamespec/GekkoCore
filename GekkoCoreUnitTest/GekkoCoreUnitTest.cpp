b8453a1e5f05faff3575bf61f8c2d2d789de0108
ogamespec <ogamespec@gmail.com>
2022-11-12 15:46:16 +0300

    Skeleton libs

---
+++ GekkoCoreUnitTest/GekkoCoreUnitTest.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace GekkoCoreUnitTest
+{
+	TEST_CLASS(GekkoCoreUnitTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
